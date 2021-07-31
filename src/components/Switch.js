ecac23522f8011dcc3cfe5882b4a1e1489e43a13
DylanDunham <dunhamdylan6258@students.esuhsd.org>
2021-07-30 22:32:17 -0700

    switch js add

---
+++ src/components/Switch.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+
+const Switch = ( ) => {
+    
+      return(
+<div>
+    <input type="checkbox"
+    class="checkbox" id="checkbox"></input>
+    <label for="checkbox"
+    class="label">
+        <i class="fas fa-moon"></i>
+        <i class="fas fa-sun"></i>
+        <div class="ball"></div>
+    </label>
+</div>
+
+      );
+  };
+  
+export default Switch;
